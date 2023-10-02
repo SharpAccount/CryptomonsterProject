d198284344fe2442989713c204964cff5eec4f81
sharpaccount <vladikinastadiki@gmail.com>
2023-10-02 16:27:55 +0300

    added token & project

---
+++ CMONProject.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "CryptoMonster.sol";
+
+contract LaunchSystem {
+    
+}
