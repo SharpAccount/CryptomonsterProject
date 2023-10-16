ccfde56cde5ab03fb76396beec8644490358cb1f
sharpaccount <vladikinastadiki@gmail.com>
2023-10-16 15:34:03 +0300

    created sign out

--- CMONProject.sol
+++ CMONProject.sol
@@ -16,26 +16,20 @@ contract LaunchSystem {
         PublicProvider
     }
 
-    // enum Statuses {
-    //     Unsigned,
-    //     Signed
-    // }
-
     struct User {
         Roles role;
-        // Statuses status;
+        address currentWallet;
     }
 
     CryptoMonster CMON = new CryptoMonster();
-    uint timeNow = block.timestamp;
     uint timeStart;
     uint timeDif;
-    uint timeSystem; //timeNow + timeDif
+    uint timeSystem; //block.timestamp + timeDif
     string signedPerson;
 
     mapping(string => User) private registeredUsers;
     mapping(string => bytes32) private logsPasses;
-    // mapping(string => Statuses) private logsStatuses;
+
 
     modifier onlyOwner {
         require(msg.sender == CMON.returnOwner(), "You don`t owner!");
@@ -51,41 +45,61 @@ contract LaunchSystem {
         }
         _;
     }
-
-    constructor() {
-        // registeredUsers[0xdD870fA1b7C4700F2BD7f44238821C26f7392148] = User("Investor1", keccak256(abi.encode("p@55W0RD")), Roles.Investor);
-        // registeredUsers[0x583031D1113aD414F02576BD6afaBfb302140225] = User("Investor2", keccak256(abi.encode("844systemUser")), Roles.Investor);
-        // registeredUsers[0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB] = User("Best friend", keccak256(abi.encode("pa55WORD")), Roles.Investor);
-        timeStart = timeNow;
-        timeSystem = timeNow;
-        timeDif = 0;
+    modifier onlyRegistered() {
+        require(bytes(signedPerson).length > 0, "You must sign in your account or register it!");
+        _;
     }
 
-    // function signOut() public onlySigned() {
+    constructor() {
+        registeredUsers["Inv1"] = User(Roles.Investor, 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        logsPasses["Inv1"] = keccak256(abi.encode("p@55W0RD"));
 
-    // }
+        registeredUsers["Inv2"] = User(Roles.Investor, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        logsPasses["Inv2"] = keccak256(abi.encode("844systemUser"));
+
+        registeredUsers["Friend"] = User(Roles.Investor, 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+        logsPasses["Friend"] = keccak256(abi.encode("pa55WORD"));
+
+        timeStart = block.timestamp;
+        timeSystem = block.timestamp;
+        timeDif = 0;
+
+        CMON.approve(registeredUsers["Inv1"].currentWallet, 300000);
+        CMON.approve(registeredUsers["Inv2"].currentWallet, 400000);
+        CMON.approve(registeredUsers["Friend"].currentWallet, 200000);
+
+        CMON.transferFrom(CMON.owner(), registeredUsers["Inv1"].currentWallet, 300000);
+        CMON.transferFrom(CMON.owner(), registeredUsers["Inv2"].currentWallet, 400000);
+        CMON.transferFrom(CMON.owner(), registeredUsers["Friend"].currentWallet, 200000);
+    }
+
     function signIn(string memory _login, string memory password) public {
         require(logsPasses[_login] == keccak256(abi.encode(password)), "Wrong login or password!");
-        //logsStatuses[_login] = Statuses.Signed;
+        require(bytes(signedPerson).length == 0, "You`re already signed!");
         signedPerson = _login;
     }
     function signUp(string memory login, string memory password) public {
-        require(registeredUsers[login].role == Roles.Guest, "Try another login!");
-        registeredUsers[login] = User(Roles.Client);
+        require(registeredUsers[login].role == Roles.Guest, "This login is busy!");
+        registeredUsers[login] = User(Roles.Client, msg.sender);
         logsPasses[login] = keccak256(abi.encode(password));
     }
+    function signOut() public onlyRegistered() {
+        signedPerson = "";
+    }
     function checkSystemLifeTime() public returns(uint) {
-        setTime();
+        resetTime();
         console.log(timeDif);
         return timeDif;
     }
-    function addTime(string memory signed) public onlyProvider(signed, 3) {
+    function addTime(string memory signed) public onlyRegistered() onlyProvider(signed, 3) {
         timeSystem += 1 minutes;
-        setTime();
+        resetTime();
     }
-    function setTime() private {
-        timeDif = timeNow - timeStart;
-        timeSystem = timeNow + timeDif;
+
+
+    function resetTime() private {
+        timeDif = block.timestamp - timeStart;
+        timeSystem = block.timestamp + timeDif;
     }
 
 }
